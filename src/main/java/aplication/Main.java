edf9b4eeed3360d26968e42cf323ff01c17e532e
Aguilera <ing.agauilera@gmail.com>
2015-12-06 13:29:37 -0500

    main

---
+++ src/main/java/aplication/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplication;
+
+/**
+ *
+ * @author user
+ */
+import GUI.GUIMain;
+
+public class Main {
+    
+    public static void Main(String args[]){
+    GUIMain gui= new GUIMain();
+}
+    
+}
